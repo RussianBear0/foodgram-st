b8704c6f847b92dc1c4efde5b2fcf88c1f01fa21
RussianBear0 <vova.denisov.19982@mail.ru>
2025-06-05 09:15:48 +0300

    Create admin.py

---
+++ backend/backend/recipes/admin.py
@@ -0,0 +1,53 @@
+from django.contrib import admin
+
+from .models import  User, Ingredient, Recipe, IngredientRecipes, UserRecipe, Follow
+
+Post, Group, Comment, Follow,
+class RecipeInline(admin.StackedInline):
+    model = Post
+    extra = 0
+
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = (
+        RecipeInline,
+    )
+
+
+class RecipeAdmin (admin.ModelAdmin):
+    model = Recipe
+    list_display = ('author',
+                    'name',
+                    'recipe_ingredients',
+                    "short_code",
+                    "text",
+                    "pub_date",
+                    )
+    search_fields = ('name',)
+    list_filter = ('pub_date',)
+
+
+class IngredientAdmin (admin.ModelAdmin):
+    model = Ingredient
+    list_display = ('name',
+                    'measure',
+                    )
+    list_editable = ("name",)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+class FollowAdmin (admin.ModelAdmin):
+    model = Follow
+    list_display = ('user',
+                    'following',
+                    )
+    list_editable = ("following",)
+    search_fields = ('user',)
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(User) 
+admin.site.register(IngredientRecipes)
+admin.site.register(UserRecipe) 
