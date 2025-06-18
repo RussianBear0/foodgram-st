bef653c4a0e98fc545620d7406400f9b6bf2d2ec
RussianBear0 <vova.denisov.19982@mail.ru>
2025-06-18 23:46:49 +0300

    Update settings.py

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -173,3 +173,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
